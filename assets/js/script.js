8c8eac979da1bca7c093ff0b026dd032f413dbd2
Kelsey Alderman <kelseyalderman@gmail.com>
2022-03-09 20:39:34 -0800

    begin populating restaurant data to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,13 +31,13 @@ var formSubmitHandler = function (event) {
 // function to get restaurants
 var getRestaurants = function (zip) {
   // format the restaurants api url
-  var apiRestarauntsUrl =
+  var apiRestaurantsUrl =
     "https://api.documenu.com/v2/restaurants/zip_code/" +
     zip +
     "?&key=eda9aa817e609c57e8a4a9e40109f7d9";
 
   // make a request to url
-  fetch(apiRestarauntsUrl)
+  fetch(apiRestaurantsUrl)
     .then(function (response) {
       // request was successful
       if (response.ok) {
@@ -98,6 +98,34 @@ let formattedDate =
   "-" +
   addLeadingZeros(currentDate.getDate());
 console.log(formattedDate);
+
+var displayRestaraunts = function (restaurantsData) {
+  // clear any previous entries in restaurant section
+  $("#restaurants").empty();
+
+  // get data for 15 restaurants
+  for (i = 1; i <= 15; i++) {
+    // create elements for restaurant card
+    var restaurantCard = $("<div>").addClass("p-10");
+    var restaurantLi = $("<li>").addClass(
+      "rounded-lg shadow-lg bg-white max-w-sm"
+    );
+
+    // append <li> to <div> restaurant card
+    restaurantCard.append(restaurantLi);
+
+    // create elements for restaurant card body
+    var restaurantCardBody = $("<div>").addClass("p-6");
+
+    // append <div> to <li>
+    restaurantLi.append(restaurantCardBody);
+
+    // create elements for restaurant card content
+    var restaurantName = $("<h5>")
+      .addClass("text-gray-800 text-xl font-medium mb-5")
+      .text(restaurantsData[i].restaurant_name);
+  }
+};
 
 // event handler
 $("#submit").on("click", formSubmitHandler);
